480a9b1d30ff195c370b5a0b09abbe48896b2271
ngust <nicholasgust@gmail.com>
2018-04-16 22:01:09 -0700

    basic devise working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+devise_for :users
 resources :rentals, only: [:show, :new, :create, :edit, :update, :index, :destroy]
 resources :locations, only: [:show, :new, :create, :edit, :update, :index, :destroy]
 
