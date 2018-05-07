3eab8d57b2c874b3ddfa4ed04638def0634a5b78
ngust <nicholasgust@gmail.com>
2018-05-06 22:23:05 -0700

    swithced bootstrap to gems, got modals working, rebuilt header to new bootstrap

--- Gemfile
+++ Gemfile
@@ -37,7 +37,6 @@ gem 'kaminari'
 gem 'carrierwave'
 gem 'fog-aws'
 gem 'dotenv-rails'
-gem 'faker'
 gem 'devise'
 gem 'bcrypt', '~> 3.1.7'
 gem 'jquery-rails'
@@ -46,6 +45,7 @@ gem "cocoon"
 gem 'faker'
 gem 'momentjs-rails'
 gem 'fullcalendar-rails'
+gem 'bootstrap', '~> 4.1.1'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -2,8 +2,27 @@ var initialize_calendar;
 initialize_calendar = function() {
   console.log("calendar initialized from file");
   $('.calendar').each(function(){
-    var calendar = $(this);
-    calendar.fullCalendar({  })
+    var calendar = $(this); 
+    calendar.fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay'
+      },
+      selectable: true,
+      selectHelper: true,
+      editable: true,
+      eventLimit: true,
+      select: function(start, end) {
+        $.getScript('/events/new', function() {
+          $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+          ;
+          $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+          $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+        });
 
+        calendar.fullCalendar('unselect');
+      },
+      })
     });
   }
