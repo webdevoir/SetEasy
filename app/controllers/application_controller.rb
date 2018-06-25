193061e0f4bcc8cf6470e6a99065da37b16339db
ngust <nicholasgust@gmail.com>
2018-06-25 15:23:54 -0700

    fixed project button issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     # before_action :authenticate_user!
-    before_action :authorize
+    # before_action :authorize
 
     delegate :allow?, to: :current_permission
 	helper_method :allow?
