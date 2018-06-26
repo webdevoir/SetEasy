81f5e5b5a592050290ebe9e6811416e1b1899941
ngust <nicholasgust@gmail.com>
2018-06-25 19:45:41 -0700

    permissions working

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -4,43 +4,50 @@ class Permission < Struct.new(:user)
 
     
     return true if user && user.role == "Admin"
-
-   
     return true if controller == "subscriptions" && action.in?(%w[new create show])
     return true if controller == "plans" && action.in?(%w[index show])
     return true if controller == "user/sessions"
     return true if controller == "user/registrations"
     return true if controller == "devise/sessions"
     return true if controller == "pages"
-    return true if controller == "projects/project_users"
+    # return true if controller == "projects/project_users"
     return true if controller == "devise/invitations"
-    return true if controller == "projects" && action.in?(%w[index show new])
+    # return true if controller == "projects" && action.in?(%w[index show new])
     
-
-    
+    if user && user.current_project
+      pro_user = ProjectUser.where(project_id: user.current_project, user_id: user.id).first
+      role = pro_user.role
+    else
+      role = nil
+    end
 
     main_ctrls = %w[crews events locations pdfs rentals ]
-    restricted = %w[budgets projects ]
+    restricted = %w[budgets ]
     # admin = %w[budgets projects admin admin/users admin/dashboard]
 
-    if user && user.role == "Production Designer"
-      return true if controller.in?(main_ctrls)
-      return true if controller.in?(restricted)
-    elsif user && user.role == "Decorator"
-      return true if controller.in?(main_ctrls)
-      return true if controller.in?(restricted)
-    elsif user && user.role == "Assistant Decorator"
-      return true if controller.in?(main_ctrls)
-      return true if controller.in?(restricted)
+    if role == "Owner"
+      return true if controller == "projects"
+      return true if controller == "projects/project_users"
 
-    elsif user && user.role == "Buyer"
+    elsif role.in?(["Production Designer", "Decorator", "Assistant Decorator"])
+      return true if controller.in?(main_ctrls)
+      return true if controller.in?(restricted)
+      return true if controller == "projects" 
+    # elsif role == "Decorator"
+    #   return true if controller.in?(main_ctrls)
+    #   return true if controller.in?(restricted)
+    # elsif role == "Assistant Decorator"
+    #   return true if controller.in?(main_ctrls)
+    #   return true if controller.in?(restricted)
+
+    elsif role.in?([ "Buyer", "Lead Dresser", "Dresser Worker"])
       return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
-    elsif user && user.role == "Lead Dresser"
-      return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
-    elsif user && user.role == "Dresser"
-      return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
-    elsif user && user.role == "Worker"
-      return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
+    # elsif user && user.role == "Lead Dresser"
+    #   return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
+    # elsif user && user.role == "Dresser"
+    #   return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
+    # elsif user && user.role == "Worker"
+    #   return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
     elsif user
       return true if controller.in?(main_ctrls)  && action.in?(%w[show index sets])
     end
