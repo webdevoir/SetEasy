9bc0caabd39e93c316fedaecc0285b2771ba1267
ngust <nicholasgust@gmail.com>
2018-07-03 14:32:26 -0700

    added create project check for new users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     before_action :authenticate_user!
     before_action :authorize
+    before_action :has_project?
 
     delegate :allow?, to: :current_permission
 	helper_method :allow?
@@ -10,7 +11,7 @@ class ApplicationController < ActionController::Base
 		logger.info "#################  LOGGED IN ##########################"
 		logger.info "#################  #{current_user.email} ##########################"
 		logger.info "#################  #{current_user.current_project} ##########################"
-		unless current_user.current_project
+		if current_user && current_user.current_project == nil && current_user.projects.length > 0
 			project = current_user.projects.first.id
 			current_user.update(current_project: project)
 		end
@@ -27,9 +28,19 @@ class ApplicationController < ActionController::Base
 
 
   def authorize
+  	  # if current_user && current_user.current_project == nil
+  	  # 	redirect_to (new_project_path, notice: "You must select a project first")
 	  if !current_permission.allow?(params[:controller], params[:action], current_resource)
 	  	redirect_back( fallback_location: root_path, notice: "Not authorized.")
 	    logger.info "#################  NOT AUTHORIZED ##########################"
 	  end
 	end
+
+  def has_project?
+	project_ctrls = %w[crews events locations pdfs rentals budgets]
+	controller = params[:controller]
+	if controller.in?(project_ctrls) && current_user && current_user.current_project == nil
+	  redirect_to projects_path, notice: "You must create a project first"
+	end
+  end
 end
