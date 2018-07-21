ce683560a146048106cb250a90e98066e8804983
ngust <nicholasgust@gmail.com>
2018-07-20 17:05:08 -0700

    changed landing page

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -3,4 +3,8 @@ class Admin::DashboardController < Admin::BaseAdminController
     	@users = User.all
     	@users = @users.order(:updated_at).reverse_order.page(params[:page]).per(15)
   	end
+
+  	def download
+    	
+  	end
 end
