fbbd4716fd441aba1ac9b0425c0de361f5ac453c
ngust <nicholasgust@gmail.com>
2018-08-04 13:13:51 -0700

    new user messages

--- app/mailers/main_mailer.rb
+++ app/mailers/main_mailer.rb
@@ -6,10 +6,16 @@ class MainMailer < ApplicationMailer
   end
 
   def contact_me(message)
-	@greeting = "Hi"
-	@body = message.body
-	@email = message.email
-	@name = message.name
-	mail to: "info@seteasy.ca", from: "info@seteasy.ca"
+  	@greeting = "Hi"
+  	@body = message.body
+  	@email = message.email
+  	@name = message.name
+  	mail to: "info@seteasy.ca", from: "info@seteasy.ca"
+  end
+
+  def send_new_user_message(user)
+    @user = user
+    @email = user.email
+    mail(to: user.email, subject: 'Welcome Aboard!', cc: 'info@seteasy.ca')
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,15 @@ class User < ApplicationRecord
   # has_many :projects, dependent: :destroy
   has_many :subscriptions, dependent: :destroy
   has_many :project_users, dependent: :destroy
-  has_many :projects, through: :project_users, dependent: :destroy 
+  has_many :projects, through: :project_users
 
 
   devise :invitable, :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+    after_create :send_admin_mail
+
+	  def send_admin_mail
+	    MainMailer.send_new_user_message(self).deliver
+	  end
 end
